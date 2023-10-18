e99284b23cee32d3a303ff94b82561e86c94b379
Juanjo <juanjose.ortizgarcia@outlook.com>
2023-10-18 19:32:57 +0200

    projects base for microservives application library

---
+++ members-service/src/test/java/com/msvc/library/membersservice/MembersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvc.library.membersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
